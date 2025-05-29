c6c0a229f5b36cc56d752c76c2d66aa80dab8d71
dinesh kumar <155614473+Dinesh222-CD@users.noreply.github.com>
2025-05-30 00:18:20 +0530

    Update Add calculator Test.java

--- Add calculator Test.java
+++ Add calculator Test.java
@@ -1,1 +1,19 @@
+package com.example;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        AddCalculator calculator = new AddCalculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        AddCalculator calculator = new AddCalculator();
+        assertEquals(1, calculator.subtract(4, 3));
+    }
+}
